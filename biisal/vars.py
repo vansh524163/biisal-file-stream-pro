b7adf196d7e56ae6b072c704f0ab86ba8ff796ab
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-06-16 11:52:47 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,15 +18,15 @@ class Var(object):
     name = str(getenv('name', 'cinema4u_vansh_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001524622686'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001524622686'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002110265321'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002065793080'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "1938030055").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "2020224264").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Akash5213'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'none_090'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
@@ -39,8 +39,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'earn_money_online_telegram_now')) 
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001524622686")).split()))   
-    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1001524622686")).split()))   
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://cinema4u:cinema4u@cluster0.ztebexy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'update_info4_bot')) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002110265321")).split()))   
+    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1002110265321")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @joinnowearn ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
