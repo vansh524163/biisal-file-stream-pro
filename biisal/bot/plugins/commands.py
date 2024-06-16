0b64c2dbbae6914e49a56be00033b0c86a440d92
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-06-16 12:02:09 +0530

    Update commands.py

--- biisal/bot/plugins/commands.py
+++ biisal/bot/plugins/commands.py
@@ -13,7 +13,7 @@ from pyrogram.errors import UserNotParticipant
 from biisal.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
-from biisal.vars import bot_name , vansh_channel , vansh_grp
+from biisal.vars import bot_name , bisal_channel , bisal_grp
 
 
 SRT_TXT = """<b>ᴊᴀɪ sʜʀᴇᴇ ᴋʀsɴᴀ {}!,
